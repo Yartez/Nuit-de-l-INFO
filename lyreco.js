1c101b837fdb3b6c65a63068be6137b9cc95a4ab
Frablock <53878484+Frablock@users.noreply.github.com>
2024-12-06 02:14:26 +0100

    Adding the lyreco lib

---
+++ lyreco.js
@@ -0,0 +1,144 @@
+/**
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░▒▒▒▒▒░░░░░▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░▒▒▒▒░░░░░▒▓▓░░░░░░░░░░░░▒▒░░▒▒░░▒▒░░░░▒▒▒▒░░░░░░░▒▒▒▒░░░░░░▒▒▒▒░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░▒▒░░░░░░▒▓▓░░░░░▓▓▓░░░▒▓▓░░▓▓▓▓▓▓░░▓▓▓▒▒▓▓▓░░░▒▓▓▓▓▓▓░░▒▓▓▓▓▓▓▓▒░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░▒▓▓░░░░░░▓▓▓░▒▓▓░░░▓▓▓░░░░▓▓▓▒▒▒▒▓▓▓░▒▓▓░░░░░░▒▓▓░░░░░▓▓▒░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░▒▓▓░░░░░░░▓▓▓▓▓▒░░░▓▓▓░░░░▓▓▓▒░░░▒░░░▒▓▓▒░░░░░▒▓▓▒░░░░▓▓▒░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░▒▓▓▓▓▓▓▓░░▒▓▓▓▒░░░░▓▓▓░░░░░▓▓▓▓▓▓▓▓░░░▒▓▓▓▓▓▓░░▒▓▓▓▓▓▓▓▒░░░░░░▒▒░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓▓▒░░░░░░░░░░░░░░░░▒▒▒░░░░░░░░▒▒▒░░░░░░▒▒▒░░░░░░░░▒▒▒▒░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒▒▒░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒▒▒░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒▒░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+Équipe COFFU
+*/
+
+const lyreco_min = 50;
+const lyreco_max = 150;
+const lyreco_div = document.createElement('div');
+
+//const animations = {0:asciiAnim};
+
+let lyreco_stop = false;
+
+lyreco_div.className = 'random-element';
+lyreco_div.id = 'Lyreco_element';
+
+lyreco_div.style.opacity = 0.25;
+lyreco_div.style.transition = "left 0.3s ease, top 0.3s ease";
+lyreco_div.style.position = "absolute";
+
+lyreco_div.onclick = triggerAnnimation;
+
+const lyreco_imageNumber = Math.floor(Math.random() * (22 + 1));
+// Crée l'élément img
+const lyreco_img = document.createElement('img');
+lyreco_img.src = './assets/'+lyreco_imageNumber+'.png';
+lyreco_img.alt = "Lyreco"
+
+lyreco_img.style.position = "absolute";
+lyreco_img.style.top = 0;
+lyreco_img.style.left = 0;
+
+// Ajoute l'image au div
+lyreco_div.appendChild(lyreco_img);
+
+// Ajoute le div au corps du document
+document.body.appendChild(lyreco_div);
+
+let lyreco_aspectRatio = 1;
+
+// Lorsque l'image est chargée
+lyreco_img.onload = function() {
+    lyreco_aspectRatio = lyreco_img.naturalWidth / lyreco_img.naturalHeight;
+
+    // Positionne l'élément aléatoirement
+    positionElementRandomly();
+
+};
+
+
+function triggerAnnimation() {
+    alert("sfuejhn");
+}
+
+function positionElementRandomly() {
+    const element = document.getElementById('Lyreco_element');
+    if (element) {
+        const position = getRandomPosition();
+        element.style.left = `${position.x}px`;
+        element.style.top = `${position.y}px`;
+
+        lyreco_img.height = Math.floor(Math.random() * (lyreco_max - lyreco_min + 1) + lyreco_min);
+        lyreco_img.width = lyreco_img.height * lyreco_aspectRatio;
+    }
+}
+
+function getRandomPosition() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    const elementWidth = Math.floor(Math.random() * (lyreco_max - lyreco_min + 1) + lyreco_min);
+    const elementHeight = Math.floor(Math.random() * (lyreco_max - lyreco_min + 1) + lyreco_min);
+
+    const randomX = Math.floor(Math.random() * (width - elementWidth));
+    const randomY = Math.floor(Math.random() * (height - elementHeight));
+
+    return { x: randomX, y: randomY };
+}
+
+function addSweating() {
+    const element = document.getElementById('Lyreco_element');
+    element.style.opacity = 0.75;
+    const img2 = document.createElement('img');
+    img2.src = './assets/sweating.png';
+    img2.alt = 'Sweating';
+
+    img2.height = img.height / 1.5;
+    img2.width = img.width / 3;
+
+    img2.style.position = "absolute";
+    img2.style.top = 0;
+    img2.style.left = 0;
+
+    element.appendChild(img2);
+}
+
+function fleeMouse(event) {
+    const element = document.getElementById('Lyreco_element');
+    if (element && !lyreco_stop) {
+        lyreco_stop = Math.floor(Math.random() * (999)) == 25;
+        const rect = element.getBoundingClientRect();
+
+        // Distance entre la souris et le centre de l'élément
+        const dx = event.clientX - (rect.left + rect.width / 2);
+        const dy = event.clientY - (rect.top + rect.height / 2);
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        // Si la souris est trop proche, repositionnez l'élément
+        if (distance < (lyreco_max+20)*lyreco_aspectRatio) { // Rayon de détection
+            const position = getRandomPosition();
+            element.style.left = `${position.x}px`;
+            element.style.top = `${position.y}px`;
+        }
+    } else if (lyreco_stop) {
+        addSweating();
+    }
+}
+
+cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
+    lyreco_stop = true;
+    addSweating();
+  });
+
+document.addEventListener('mousemove', fleeMouse);
+
+// Positionne l'élément aléatoirement au chargement de la page
+window.onresize = positionElementRandomly;
+window.onscroll = positionElementRandomly;
+window.onchange = positionElementRandomly;
